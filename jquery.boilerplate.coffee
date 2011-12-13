8c3e7f2b78c2a8032d07a341bbb1e571c1ce947a
Zeno Rocha <contato@zenorocha.com>
2011-12-13 21:48:43 -0200

    creating a new branch for the french version

--- jquery.boilerplate.coffee
+++ jquery.boilerplate.coffee
@@ -1,33 +1,31 @@
-#  Projet:
+#  Project:
 #  Description:
-#  Auteur:
-#  Licence:
+#  Author:
+#  License:
 
-# Le point-virgule avant l'invocation de la fonction est un filet de sécurité
-# si le plugin est concaténé avec d'autre scripts et/ou d'autres plugin qui
-# pourraient ne pas avoir été convenablement fermés.
+# the semi-colon before function invocation is a safety net against concatenated
+# scripts and/or other plugins which may not be closed properly.
 ``
-# À noter que compilé avec Coffeescript, le plugin est automatiquement
-# encapsulé dans une fonction anonyme. Nous n'avons pas non plus besoin de lui
-# passer "undefined", puisque Coffeescript utilise "void 0" à la place.
+# Note that when compiling with coffeescript, the plugin is wrapped in another
+# anonymous function. We do not need to pass in undefined as well, since
+# coffeescript uses (void 0) instead.
 (($, window, document) ->
-  # "window" et "document" sont passé par des variables locales plutôt que par
-  # les globales, cela accélère (relativement) le processus de résolution et
-  # permet de mieux tirer avantage de la "minification" (tout particulièrement
-  # quand les deux sont régulièrement référencés dans votre plugin).
+  # window and document are passed through as local variables rather than globals
+  # as this (slightly) quickens the resolution process and can be more efficiently
+  # minified (especially when both are regularly referenced in your plugin).
 
-  # Créons celui par défaut
+  # Create the defaults once
   pluginName = 'defaultPluginName'
   defaults =
     property: 'value'
 
-  # Voici le constructeur du plugin
+  # The actual plugin constructor
   class Plugin
     constructor: (@element, options) ->
-      # jQuery a une méthode "extend" qui fusionne les contenus d'au moins
-      # deux objets, sauvegardant le résultat dans le premier. Le premier objet
-      # est généralement vide, vu que nous ne souhaitons pas altérer les options
-      # par défaut des futures instances du plugin.
+      # jQuery has an extend method which merges the contents of two or
+      # more objects, storing the result in the first object. The first object
+      # is generally empty as we don't want to alter the default options for
+      # future instances of the plugin
       @options = $.extend {}, defaults, options
 
       @_defaults = defaults
@@ -36,12 +34,12 @@
       @init()
 
     init: ->
-      # Mettez votrre logique d'initialisation ici
-      # Vous avez déjà accès à l'élément DOM et les options via l'instance,
-      # par ex. : this.element et this.options
+      # Place initialization logic here
+      # You already have access to the DOM element and the options via the instance,
+      # e.g., this.element and this.options
 
-  # Un très léger décorateur autour du constructeur, pour en éviter de multiples
-  # instanciations.
+  # A really lightweight plugin wrapper around the constructor,
+  # preventing against multiple instantiations
   $.fn[pluginName] = (options) ->
     @each ->
       if !$.data(this, "plugin_#{pluginName}")
--- jquery.boilerplate.js
+++ jquery.boilerplate.js
@@ -1,49 +1,38 @@
-/**
- * Projet:
- * Description:
- * Auteur:
- * Licence:
+/*
+ *  Project: 
+ *  Description: 
+ *  Author: 
+ *  License: 
  */
 
-/**
- * Le point-virgule avant l'invocation de la fonction est un filet de sécurité
- * si le plugin est concaténé avec d'autre scripts et/ou d'autres plugin qui
- * pourraient ne pas avoir été convenablement fermés.
- */
+
+// the semi-colon before function invocation is a safety net against concatenated 
+// scripts and/or other plugins which may not be closed properly.
 ;(function ( $, window, document, undefined ) {
     
-    /**
-     * "undefined" est utilisé ici car sa variable globale est mutable en
-     * ECMAScript 3, c'est-à-dire qu'elle peut être redéfinie par quelqu'un
-     * d'autre. De plus, "undefined" n'est pas réélement passé à la
-     * fonction, ainsi nous sommes certains que sa valeure est bien "undefined"
-     * comme un "void 0". D'autre part en ES5, "undefined" n'est plus mutable.
-     */
+    // undefined is used here as the undefined global variable in ECMAScript 3 is
+    // mutable (ie. it can be changed by someone else). undefined isn't really being
+    // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
+    // can no longer be modified.
     
-    /**
-     * "window" et "document" sont passé par des variables locales plutôt que
-     * par les globales, cela accélère (relativement) le processus de résolution
-     * et permet de mieux tirer avantage de la "minification" (tout
-     * particulièrement quand les deux sont régulièrement référencés dans votre
-     * plugin).
-     */
+    // window and document are passed through as local variables rather than globals
+    // as this (slightly) quickens the resolution process and can be more efficiently
+    // minified (especially when both are regularly referenced in your plugin).
 
-    // Créons celui par défaut
+    // Create the defaults once
     var pluginName = 'defaultPluginName',
         defaults = {
             propertyName: "value"
         };
 
-    // Voici le constructeur du plugin
+    // The actual plugin constructor
     function Plugin( element, options ) {
         this.element = element;
 
-        /**
-         * jQuery a une méthode "extend" qui fusionne les contenus d'au moins
-         * deux objets, sauvegardant le résultat dans le premier. Le premier
-         * objet est généralement vide, vu que nous ne souhaitons pas altérer
-         * les options par défaut des futures instances du plugin.
-         */
+        // jQuery has an extend method which merges the contents of two or 
+        // more objects, storing the result in the first object. The first object
+        // is generally empty as we don't want to alter the default options for
+        // future instances of the plugin
         this.options = $.extend( {}, defaults, options) ;
         
         this._defaults = defaults;
@@ -53,17 +42,13 @@
     }
 
     Plugin.prototype.init = function () {
-      /**
-       * Mettez votre logique d'initialisation ici
-       * Vous avez déjà accès à l'élément DOM et les options via l'instance,
-       * par ex. : this.element et this.options
-       */
+        // Place initialization logic here
+        // You already have access to the DOM element and the options via the instance, 
+        // e.g., this.element and this.options
     };
 
-    /**
-     * Un très léger décorateur autour du constructeur, pour en éviter de
-     * multiples instanciations.
-     */
+    // A really lightweight plugin wrapper around the constructor, 
+    // preventing against multiple instantiations
     $.fn[pluginName] = function ( options ) {
         return this.each(function () {
             if (!$.data(this, 'plugin_' + pluginName)) {
