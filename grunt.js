d95a720c64b4834b71b626c4f1987b65af514472
William Oliveira <sudowilliam@gmail.com>
2012-09-16 23:13:41 -0300

    Changing and updating tasks

--- grunt.js
+++ grunt.js
@@ -7,6 +7,12 @@ module.exports = function(grunt) {
     lint: {
       all: ['grunt.js', '*.js']
     },
+    min: {
+      dist: {
+        src: ['jquery.boilerplate.js'],
+        dest: 'jquery.boilerplate.min.js'
+      }
+    },
     jshint: {
       globals: {
         jQuery: true
@@ -14,6 +20,8 @@ module.exports = function(grunt) {
     }
   });
 
-  // registers the task and what it should do
-  grunt.registerTask('default', 'qunit lint');
+  // tasks
+  grunt.registerTask('default', 'lint');
+  grunt.registerTask('test', 'qunit');
+  grunt.registerTask('min', 'min');
 };
