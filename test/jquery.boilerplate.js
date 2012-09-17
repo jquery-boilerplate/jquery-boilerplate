b9a43e752d24fe1f353888b3cf12cd4e33996936
William Oliveira <sudowilliam@gmail.com>
2012-09-16 23:13:58 -0300

    Changing module to double quotes

--- test/jquery.boilerplate.js
+++ test/jquery.boilerplate.js
@@ -1,6 +1,6 @@
 (function($) {
 
-  module('jQuery-Boilerplate');
+  module("jQuery-Boilerplate");
 
   test("should be defined on jquery object", function () {
     ok($(document.body).defaultPluginName, 'defaultPluginName method is defined');
