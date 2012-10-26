724388f057b765b4de2925e262fc098cbe441b6c
DiegoLopesLima <diego.lopes.lima@bol.com.br>
2012-10-26 16:24:41 -0200

    Eliminei a redundancia ao extender jQuery

--- jquery.boilerplate.js
+++ jquery.boilerplate.js
@@ -20,44 +20,38 @@
 			methodName : {
 				propertyName : 'value'
 			}
-		},
-
-		// Métodos a extender jQuery.fn
-		fnExtend = {
-
-			// Cria um método do plugin
-			methodName : function(options) {
-
-				// Recebe os parâmetros passados pelo usuário em um objeto sobrepondo as propriedades padrões caso redeclaradas
-				options = $.isPlainObject(options) && !$.isEmptyObject(options) ? $.extend(defaults.methodName, options) : defaults.methodName;
-
-				// Retorna o próprio elemento encapsulado para proporcionar encadeamento
-				return $(this).each(function() {
-
-					// Corpo do plugin
-					// O contexto atual (this) é o próprio elemento encapsulado percorrido atualmente
-					// Sintaxe de uso: jQuery(fn).methodName(options);
-				});
-			}
-		},
-
-		// Métodos a extender jQuery
-		extend = {
-
-			// Cria um método para alterar os valores padrões do plugin
-			// Sintaxe de uso: jQuery.methodName(options);
-			methodNameSetup : function(options) {
-
-				// Retorna o próprio objeto modificado
-				return defaults.methodName = $.isPlainObject(options) && !$.isEmptyObject(options) ? $.extend(defaults.methodName, options) : defaults.methodName;
-			},
-
-			// Cria um objeto para guardar informações sobre o plugin (Neste caso a versão atual)
-			methodNameInfo : {
-				version : '1b'
-			}
 		};
 
 	// Extende a biblioteca jQuery
-	$.extend(extend).fn.extend(fnExtend);
+	$.extend({
+
+		// Cria um método para alterar os valores padrões do plugin
+		// Sintaxe de uso: jQuery.methodName(options);
+		methodNameSetup : function(options) {
+
+			// Retorna o próprio objeto modificado
+			return defaults.methodName = $.isPlainObject(options) && !$.isEmptyObject(options) ? $.extend(defaults.methodName, options) : defaults.methodName;
+		},
+
+		// Cria um objeto para guardar informações sobre o plugin (Neste caso a versão atual)
+		methodNameInfo : {
+			version : '1b'
+		}
+	}).fn.extend({
+
+		// Cria um método do plugin
+		methodName : function(options) {
+
+			// Recebe os parâmetros passados pelo usuário em um objeto sobrepondo as propriedades padrões caso redeclaradas
+			options = $.isPlainObject(options) && !$.isEmptyObject(options) ? $.extend(defaults.methodName, options) : defaults.methodName;
+
+			// Retorna o próprio elemento encapsulado para proporcionar encadeamento
+			return $(this).each(function() {
+
+				// Corpo do plugin
+				// O contexto atual (this) é o próprio elemento encapsulado percorrido atualmente
+				// Sintaxe de uso: jQuery(fn).methodName(options);
+			});
+		}
+	});
 })(jQuery, window);
