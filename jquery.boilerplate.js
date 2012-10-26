ff87b88f093b784f1930d25623bc801c5d5ccaa8
DiegoLopesLima <diego.lopes.lima@bol.com.br>
2012-10-26 09:41:08 -0200

    Atualizacao dos comentarios

--- jquery.boilerplate.js
+++ jquery.boilerplate.js
@@ -6,7 +6,10 @@
  * @version : 1 beta
  * @required : jQuery 1.4+
  */
-;(function($, window) {
+(function($, window) {
+
+	// A váriavel local $ recebe jQuery para evitar conflitos com outras bibliotecas
+	// window passa a ser variável local para melhor desempenho
 
 	var
 		// Adota document como variável local para melhor desempenho
@@ -19,25 +22,33 @@
 			}
 		},
 
+		// Métodos a extender jQuery.fn
 		fnExtend = {
 
 			// Cria um método do plugin
 			methodName : function(options) {
 
+				// Recebe os parâmetros passados pelo usuário em um objeto sobrepondo as propriedades padrões caso redeclaradas
 				options = $.isPlainObject(options) && !$.isEmptyObject(options) ? $.extend(defaults.methodName, options) : defaults.methodName;
 
+				// Retorna o próprio elemento encapsulado para proporcionar encadeamento
 				return $(this).each(function() {
 
-					//
+					// Corpo do plugin
+					// O contexto atual (this) é o próprio elemento encapsulado percorrido atualmente
+					// Sintaxe de uso: jQuery(fn).methodName(options);
 				});
 			}
 		},
 
+		// Métodos a extender jQuery
 		extend = {
 
 			// Cria um método para alterar os valores padrões do plugin
+			// Sintaxe de uso: jQuery.methodName(options);
 			methodNameSetup : function(options) {
 
+				// Retorna o próprio objeto modificado
 				return defaults.methodName = $.isPlainObject(options) && !$.isEmptyObject(options) ? $.extend(defaults.methodName, options) : defaults.methodName;
 			},
 
@@ -47,5 +58,6 @@
 			}
 		};
 
+	// Extende a biblioteca jQuery
 	$.extend(extend).fn.extend(fnExtend);
 })(jQuery, window);
