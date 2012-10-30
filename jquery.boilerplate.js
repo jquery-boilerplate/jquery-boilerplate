0fd3f22447a278aec4ae48ff2dd1fa62b2b9b6d5
felquis <ofelquis@hotmail.com>
2012-10-30 21:55:33 -0200

    Add variavel myLogic

--- jquery.boilerplate.js
+++ jquery.boilerplate.js
@@ -8,7 +8,7 @@
 // the semi-colon before function invocation is a safety net against concatenated
 // scripts and/or other plugins which may not be closed properly.
 ;(function ( $, window, undefined ) {
-    
+
     // undefined is used here as the undefined global variable in ECMAScript 3 is
     // mutable (ie. it can be changed by someone else). undefined isn't really being
     // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
@@ -23,7 +23,14 @@
         document = window.document,
         defaults = {
             propertyName: "value"
-        };
+        },
+        myLogic = {
+            init : function () {
+                // Place initialization logic here
+                // You already have access to the DOM element and the options via the instance,
+                // e.g., this.element and this.options
+            }
+        }
 
     // The actual plugin constructor
     function Plugin( element, options ) {
@@ -41,11 +48,7 @@
         this.init();
     }
 
-    Plugin.prototype.init = function () {
-        // Place initialization logic here
-        // You already have access to the DOM element and the options via the instance,
-        // e.g., this.element and this.options
-    };
+    Plugin.prototype = myLogic;
 
     // A really lightweight plugin wrapper around the constructor,
     // preventing against multiple instantiations
