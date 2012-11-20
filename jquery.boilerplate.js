6088ba3e09224a1d0f0535bf91eb8395f6e0c081
Zeno Rocha <zno.rocha@gmail.com>
2012-11-20 12:49:43 -0200

    removing trailling spaces

--- jquery.boilerplate.js
+++ jquery.boilerplate.js
@@ -1,19 +1,19 @@
 /*
- *  Project: 
- *  Description: 
- *  Author: 
- *  License: 
+ *  Project:
+ *  Description:
+ *  Author:
+ *  License:
  */
 
 // the semi-colon before function invocation is a safety net against concatenated
 // scripts and/or other plugins which may not be closed properly.
 ;(function ( $, window, undefined ) {
-    
+
     // undefined is used here as the undefined global variable in ECMAScript 3 is
     // mutable (ie. it can be changed by someone else). undefined isn't really being
     // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
     // can no longer be modified.
-    
+
     // window is passed through as local variable rather than global
     // as this (slightly) quickens the resolution process and can be more efficiently
     // minified (especially when both are regularly referenced in your plugin).
@@ -34,10 +34,10 @@
         // is generally empty as we don't want to alter the default options for
         // future instances of the plugin
         this.options = $.extend( {}, defaults, options) ;
-        
+
         this._defaults = defaults;
         this._name = pluginName;
-        
+
         this.init();
     }
 
