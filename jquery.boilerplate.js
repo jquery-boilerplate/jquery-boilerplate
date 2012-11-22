52634f1dde1fdb9407cc30319f04dab8d4290b9f
Zeno Rocha <zno.rocha@gmail.com>
2012-11-22 02:37:17 -0200

    improving code comments for document variable

--- jquery.boilerplate.js
+++ jquery.boilerplate.js
@@ -14,7 +14,7 @@
     // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
     // can no longer be modified.
 
-    // window is passed through as local variable rather than global
+    // window and document are passed through as local variable rather than global
     // as this (slightly) quickens the resolution process and can be more efficiently
     // minified (especially when both are regularly referenced in your plugin).
 
