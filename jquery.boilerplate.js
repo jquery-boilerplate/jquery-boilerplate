b2bf5abb14be9825a43ec4a61748218e7901824d
Matheus Kautzmann <kautzmann5@gmail.com>
2013-02-12 08:11:34 -0200

    Traduzindo o código fonte para pt-br

--- jquery.boilerplate.js
+++ jquery.boilerplate.js
@@ -1,12 +1,15 @@
 /*
- *  Project:
- *  Description:
- *  Author:
- *  License:
+ *  Projeto:
+ *  Descrição:
+ *  Autor:
+ *  Licença:
  */
 
 // the semi-colon before function invocation is a safety net against concatenated
 // scripts and/or other plugins which may not be closed properly.
+
+// O ponto-e-vírgula antes da invocação da função é uma segurança contra scripts
+// concatenados e/ou outros plugins que possam não estar devidamente fechados.
 ;(function ($, window, document, undefined) {
 
     // undefined is used here as the undefined global variable in ECMAScript 3 is
@@ -14,23 +17,28 @@
     // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
     // can no longer be modified.
 
-    // window and document are passed through as local variable rather than global
-    // as this (slightly) quickens the resolution process and can be more efficiently
-    // minified (especially when both are regularly referenced in your plugin).
+    // undefined é usado aqui pois a variável global undefined no ECMAScript 3 é
+    // mutável (isto é, pode ser alterada por outro script). Esta variável não está
+    // realmente sendo passada assim podemos garantir que seu valor é realmente
+    // indefinido. No ES5, a variável undefined não pode mais ser modificada.
 
-    // Create the defaults once
+    // window e document são passadas através de variável local em vez de global,
+    // isso melhora o processo de resolução e pode ser mais eficiente quando o
+    // código é minificado (especialmente quando são muito utilizdas no seu plugin).
+
+    // Cria os defaults somente uma vez
     var pluginName = "defaultPluginName";
     var defaults = {
         propertyName: "value"
     };
 
-    // The actual plugin constructor
+    // O construtor do plugin
     function Plugin(element, options) {
         this.element = element;
-        // jQuery has an extend method which merges the contents of two or
-        // more objects, storing the result in the first object. The first object
-        // is generally empty as we don't want to alter the default options for
-        // future instances of the plugin
+        // O jQuery tem um método chamado extend que junta os conteúdos de dois ou
+        // mais objetos, guardando o resultado no primeiro objeto. O primeiro objeto
+        // geralmente é vazio pois não queremos alterar as opções default para as
+        // futuras instâncias do plugin.
         this.options = $.extend({}, defaults, options);
         this._defaults = defaults;
         this._name = pluginName;
@@ -39,20 +47,20 @@
 
     Plugin.prototype = {
         init: function () {
-            // Place initialization logic here
-            // You already have access to the DOM element and
-            // the options via the instance, e.g. this.element
-            // and this.options
-            // you can add more functions like the one below and
-            // call them like so: this.yourOtherFunction(this.element, this.options).
+            // Coloque sua lógica de inicialização aqui.
+            // Você já deve ter acesso ao elemento do DOM e
+            // às opções da instância, e.g. this.element
+            // e this.options
+            // você pode adicionar mais funções como esta abaixo e
+            // chamá-las como: this.suaOutraFuncao(this.element, this.options).
         },
-        yourOtherFunction: function () {
-            // some logic
+        suaOutraFuncao: function () {
+            // Alguma outra lógica
         }
     };
 
-    // A really lightweight plugin wrapper around the constructor,
-    // preventing against multiple instantiations
+    // Um encapsulamento muito leve entorno do construtor,
+    // prevenindo contra múltiplas instanciações.
     $.fn[pluginName] = function (options) {
         return this.each(function () {
             if (!$.data(this, "plugin_" + pluginName)) {
