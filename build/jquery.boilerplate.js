6149027c81ce4ca6b157043651fbb9bc1fc8f3e3
Zeno Rocha <zno.rocha@gmail.com>
2013-05-01 09:56:51 -0300

    Use jQuery's Core style

--- build/jquery.boilerplate.js
+++ build/jquery.boilerplate.js
@@ -8,7 +8,7 @@
  */
 // the semi-colon before function invocation is a safety net against concatenated
 // scripts and/or other plugins which may not be closed properly.
-;(function ($, window, document, undefined) {
+;(function ( $, window, document, undefined ) {
 
 		// undefined is used here as the undefined global variable in ECMAScript 3 is
 		// mutable (ie. it can be changed by someone else). undefined isn't really being
@@ -26,13 +26,13 @@
 		};
 
 		// The actual plugin constructor
-		function Plugin(element, options) {
+		function Plugin ( element, options ) {
 				this.element = element;
 				// jQuery has an extend method which merges the contents of two or
 				// more objects, storing the result in the first object. The first object
 				// is generally empty as we don't want to alter the default options for
 				// future instances of the plugin
-				this.options = $.extend({}, defaults, options);
+				this.options = $.extend( {}, defaults, options );
 				this._defaults = defaults;
 				this._name = pluginName;
 				this.init();
@@ -54,12 +54,12 @@
 
 		// A really lightweight plugin wrapper around the constructor,
 		// preventing against multiple instantiations
-		$.fn[pluginName] = function (options) {
-				return this.each(function () {
-						if (!$.data(this, "plugin_" + pluginName)) {
-								$.data(this, "plugin_" + pluginName, new Plugin(this, options));
+		$.fn[ pluginName ] = function ( options ) {
+				return this.each(function() {
+						if ( !$.data( this, "plugin_" + pluginName ) ) {
+								$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
 						}
 				});
 		};
 
-})(jQuery, window, document);
+})( jQuery, window, document );
