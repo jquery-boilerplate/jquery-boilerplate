045a7f2381dbfcf483023f018dee31eaf6f90391
Zeno Rocha <zno.rocha@gmail.com>
2013-05-01 09:31:44 -0300

    Add Grunt v0.4 tasks

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    // Import package manifest
+    pkg: grunt.file.readJSON('boilerplate.jquery.json'),
+
+    // Banner definitions
+    meta: {
+      banner: '/*\n' +
+        ' *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n' +
+        ' *  <%= pkg.description %>\n' +
+        ' *  <%= pkg.homepage %>\n' +
+        ' *\n' +
+        ' *  Copyright (c) <%= grunt.template.today("yyyy") %>\n' +
+        ' *  MIT License\n' +
+        ' */\n'
+    },
+
+		// Concat definitions
+    concat: {
+      dist: {
+        src: ['src/jquery.boilerplate.js'],
+        dest: 'build/jquery.boilerplate.js'
+      },
+      options: {
+        banner: '<%= meta.banner %>'
+      }
+    },
+
+    // Lint definitions
+    jshint: {
+    	files: ['src/jquery.boilerplate.js'],
+    	options: {
+        jshintrc: ".jshintrc"
+      }
+    },
+
+    // Minify definitions
+    uglify: {
+      my_target: {
+        src: ['build/jquery.boilerplate.js'],
+        dest: 'build/jquery.boilerplate.min.js'
+      },
+      options: {
+        banner: '<%= meta.banner %>'
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+  grunt.registerTask('travis', ['jshint']);
+
+};
