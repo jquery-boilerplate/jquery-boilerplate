8202d5f22e94cf9c96c5e6b25cdd17169eea657e
Zeno Rocha <zno.rocha@gmail.com>
2013-05-10 23:05:58 -0300

    Switch from single to double quotes

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,34 +3,34 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 
 		// Import package manifest
-		pkg: grunt.file.readJSON('boilerplate.jquery.json'),
+		pkg: grunt.file.readJSON("boilerplate.jquery.json"),
 
 		// Banner definitions
 		meta: {
-			banner: '/*\n' +
-				' *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n' +
-				' *  <%= pkg.description %>\n' +
-				' *  <%= pkg.homepage %>\n' +
-				' *\n' +
-				' *  Made by <%= pkg.author.name %>\n' +
-				' *  Under <%= pkg.licenses[0].type %> License\n' +
-				' */\n'
+			banner: "/*\n" +
+				" *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
+				" *  <%= pkg.description %>\n" +
+				" *  <%= pkg.homepage %>\n" +
+				" *\n" +
+				" *  Made by <%= pkg.author.name %>\n" +
+				" *  Under <%= pkg.licenses[0].type %> License\n" +
+				" */\n"
 		},
 
 		// Concat definitions
 		concat: {
 			dist: {
-				src: ['src/jquery.boilerplate.js'],
-				dest: 'dist/jquery.boilerplate.js'
+				src: ["src/jquery.boilerplate.js"],
+				dest: "dist/jquery.boilerplate.js"
 			},
 			options: {
-				banner: '<%= meta.banner %>'
+				banner: "<%= meta.banner %>"
 			}
 		},
 
 		// Lint definitions
 		jshint: {
-			files: ['src/jquery.boilerplate.js'],
+			files: ["src/jquery.boilerplate.js"],
 			options: {
 				jshintrc: ".jshintrc"
 			}
@@ -39,11 +39,11 @@ module.exports = function(grunt) {
 		// Minify definitions
 		uglify: {
 			my_target: {
-				src: ['dist/jquery.boilerplate.js'],
-				dest: 'dist/jquery.boilerplate.min.js'
+				src: ["dist/jquery.boilerplate.js"],
+				dest: "dist/jquery.boilerplate.min.js"
 			},
 			options: {
-				banner: '<%= meta.banner %>'
+				banner: "<%= meta.banner %>"
 			}
 		},
 
@@ -51,19 +51,19 @@ module.exports = function(grunt) {
 		coffee: {
 			compile: {
 				files: {
-					'dist/jquery.boilerplate.js': 'src/jquery.boilerplate.coffee'
+					"dist/jquery.boilerplate.js": "src/jquery.boilerplate.coffee"
 				}
 			}
 		}
 
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-coffee');
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-coffee");
 
-	grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
-	grunt.registerTask('travis', ['jshint']);
+	grunt.registerTask("default", ["jshint", "concat", "uglify"]);
+	grunt.registerTask("travis", ["jshint"]);
 
 };
