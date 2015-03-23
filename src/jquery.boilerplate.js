9a0fe2efb5c91941a97da2b1388b6d4268ea30c0
Francisco Kahil <codexico@gmail.com>
2015-03-23 00:47:38 -0300

    commenting before the code

--- src/jquery.boilerplate.js
+++ src/jquery.boilerplate.js
@@ -1,17 +1,19 @@
+// https://github.com/jquery-boilerplate/jquery-boilerplate/blob/master/src/jquery.boilerplate.js
+//
 // the semi-colon before function invocation is a safety net against concatenated
 // scripts and/or other plugins which may not be closed properly.
+//
+// undefined is used here as the undefined global variable in ECMAScript 3 is
+// mutable (ie. it can be changed by someone else). undefined isn't really being
+// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
+// can no longer be modified.
+//
+// window and document are passed through as local variable rather than global
+// as this (slightly) quickens the resolution process and can be more efficiently
+// minified (especially when both are regularly referenced in your plugin).
 ;(function ( $, window, document, undefined ) {
 
 	"use strict";
-
-		// undefined is used here as the undefined global variable in ECMAScript 3 is
-		// mutable (ie. it can be changed by someone else). undefined isn't really being
-		// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
-		// can no longer be modified.
-
-		// window and document are passed through as local variable rather than global
-		// as this (slightly) quickens the resolution process and can be more efficiently
-		// minified (especially when both are regularly referenced in your plugin).
 
 		// Create the defaults once
 		var pluginName = "defaultPluginName",
