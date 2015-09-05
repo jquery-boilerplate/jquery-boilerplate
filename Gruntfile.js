c32b64569bbb641bf9f51e6bd74bf411b0128888
Helder Santana <helder.css@gmail.com>
2015-09-05 01:00:55 -0400

    update demo file and fix headless test

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,13 +62,13 @@ module.exports = function(grunt) {
 		    configFile: "karma.conf.js",
 				background: true,
 				singleRun: false,
-				browsers: ["PhantomJS"]
+				browsers: ["PhantomJS", "Firefox"]
 		  },
 		  //continuous integration mode: run tests once in PhantomJS browser.
 		  travis: {
 		    configFile: "karma.conf.js",
 		    singleRun: true,
-		    browsers: ["PhantomJS", "Firefox"]
+		    browsers: ["PhantomJS"]
 		  },
 		},
 
